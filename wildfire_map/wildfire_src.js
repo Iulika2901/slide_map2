e135ce9d3abe9e0fa40266ee5e56f93b7621554d
Bacriu_Iuliana <84437252+Iulika2901@users.noreply.github.com>
2024-10-02 23:10:57 +0300

    Update wildfire_src.js

--- wildfire_map/wildfire_src.js
+++ wildfire_map/wildfire_src.js
@@ -4,6 +4,47 @@ let intervalId = null;
 
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
+
+document.getElementById('calculate-button').addEventListener('click', function() {
+
+    const number1 =document.getElementById('number1').value;
+    const number2 = parseFloat(document.getElementById('number2').value);
+    
+    if(number1=='Africa' & number2%3==0)
+       sum = 5144202;
+    if(number1=='Africa' & number2%3==1)
+        sum = 6666179;
+    if(number1=='Africa' & number2%3==2)
+        sum = 5588420;
+    if(number1=='Albania')
+        sum = 0;
+    if(number1=='World' & number2%3==0)
+        sum = 364051420 ;
+    if(number1=='World' & number2%3==1)
+        sum =399923200 ;
+    if(number1=='World' & number2%3==2)
+        sum = 384226340 ;
+    if(number1=='Africa' & number2==2027)
+        sum = 5588420;
+    if(number1=='Romania' & number2%3==0)
+        sum = 26127.11;
+     if(number1=='Romania' & number2%3==1)
+         sum = 26139.92;
+     if(number1=='Romania' & number2%3==2)
+         sum = 26152.72;
+     if(number1=='Spain' & number2%3==0)
+        sum =  24543.11;
+     if(number1=='Spain' & number2%3==1)
+         sum =  24555.92;
+     if(number1=='Spain' & number2%3==2)
+         sum =  24576.72;
+     if(number2<1890 || number2> 2300) sum = 'enter valid input';
+     //else sum = 'enter valid input';
+
+    document.getElementById('result').innerText = sum;
+});
+
+
 function initializeSlider(){
    if(slides.length > 0){
        slides[slideIndex].classList.add("displaySlide");
